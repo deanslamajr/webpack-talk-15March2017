5fe83c7ed77b7587570c13aee90f8e2c20c08ff6
Dean Slama Jr <deanslamajr@gmail.com>
2017-03-12 17:58:40 -0500

    2.0

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-var message = 'hello world';
+// create buttons
+var buttonElement = document.createElement('input');
+buttonElement.type = 'button';
+buttonElement.value = 'hello world';
+var buttonElement2 = document.createElement('input');
+buttonElement2.type = 'button';
+buttonElement2.value = 'hello world 2';
 
-function displayMessage() {
+// add behaviors to buttons
+buttonElement.onclick = doAlert;
+buttonElement2.onclick = doAlert2;
+
+// add buttons to DOM
+document.body.appendChild(buttonElement);
+document.body.appendChild(buttonElement2);
+
+
+// alerts
+var message = 'hello world!';
+var message2 = 'hello world again!';
+
+function doAlert() {
   alert(message);
 }
 
-displayMessage();
+function doAlert2() {
+  alert(message2);
+}
